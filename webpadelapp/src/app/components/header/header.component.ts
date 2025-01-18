ae6da8e39c8862155d4534f78c7bd78588ae87c0
Jimmy Coque Yanez <jimmycoqueyanez1991@gmail.com>
2025-01-19 00:17:36 +0100

    modified html and ts  header

--- webpadelapp/src/app/components/header/header.component.ts
+++ webpadelapp/src/app/components/header/header.component.ts
@@ -9,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  categories = [
+    {
+      name: 'Palas de padel',
+      subcategories: ['Adidas', 'Bullpadel', 'Babolat'],
+      open: false,
+    },
+    {
+      name: 'Accesorios',
+      subcategories: ['Bolsas', 'Calzados'],
+      open: false,
+    },
+  ];
+
+  toggleSubmenu(categoryName: string): void {
+    this.categories = this.categories.map((cat) =>
+      cat.name === categoryName ? { ...cat, open: !cat.open } : { ...cat, open: false }
+    );
+  }
 }
--- webpadelapp/src/main.ts
+++ webpadelapp/src/main.ts
@@ -1,6 +1,28 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+
+//Original: 
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // Nuevo enfoque
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/components/home/home.component';
+import { CategoryComponent } from './app/components/category/category.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta principal
+  { path: 'category/:subcategory', component: CategoryComponent }, // Ruta dinámica para categorías
+  { path: '**', redirectTo: '' }, // Redirección por defecto
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes), // Configuración de rutas
+    provideHttpClient(), // Configuración del cliente HTTP
+  ],
+});
