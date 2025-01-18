63a1b21811fbc90f401b4f1136c9927f8957716d
Jimmy Coque Yanez <jimmycoqueyanez1991@gmail.com>
2025-01-19 00:19:59 +0100

    modifie html and ts category

--- webpadelapp/src/app/components/category/category.component.ts
+++ webpadelapp/src/app/components/category/category.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-category',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './category.component.html',
+//   styleUrl: './category.component.css'
+// })
+// export class CategoryComponent {
+
+// }
+
+//ORIGINAL 
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrls: ['./category.component.css'],
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categoryName: string | null = '';
+  products: any[] | null = null;
 
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.categoryName = this.route.snapshot.paramMap.get('subcategory');
+    if (this.categoryName) {
+      this.fetchProducts(this.categoryName);
+    }
+  }
+
+  fetchProducts(subcategory: string): void {
+    this.http
+      .get<any[]>(`http://localhost:3000/api/products?subcategory=${subcategory}`)
+      .subscribe((data) => (this.products = data));
+  }
 }
