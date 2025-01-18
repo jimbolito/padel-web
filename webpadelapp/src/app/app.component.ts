de24e01d039b21aef5bd1e2acce843c9a2920c62
Jimmy Coque Yanez <jimmycoqueyanez1991@gmail.com>
2025-01-19 00:24:08 +0100

    added components in app.component.ts file

--- webpadelapp/src/app/app.component.ts
+++ webpadelapp/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `,
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'webpadelapp';
-}
+export class AppComponent {}
